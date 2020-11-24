086fa8a4955597c024f2c1f7ae425f9b9253ca9a
manae <manae@test.com>
2020-11-24 08:38:24 +0000

    fix line messages send func

--- lineAPI/app/controllers/send_line_controller.rb
+++ lineAPI/app/controllers/send_line_controller.rb
@@ -1,13 +1,13 @@
 class SendLineController < ApplicationController
   def messages
-    form_data = post_param
+    return head 400 unless form_data = post_param
     req = send_api(:line, 'v2/bot/message/push', :post, content_type: 'application/json', form_data: form_data)    
   end
 
   private
     def post_param
-      head 400; return if params[:to].nil? || params[:messages].nil?
-      head 400; return unless params[:messages].is_a?(Array)
+      return false if params[:to].nil? || params[:messages].nil?
+      return false unless params[:messages].is_a?(Array)
 
       hash = params.permit(:to).to_hash
       hash["messages"] = params.require(:messages).map do |message|
